fe75a8cf71c056d2058147d346277f0685bdf15e
Mintesnot Bekele <102607058+mintesnotbekele@users.noreply.github.com>
2025-05-19 17:40:14 +0300

    coming soon added on the ecommerce

--- src/pages/ecommerce/products/ProductList.tsx
+++ src/pages/ecommerce/products/ProductList.tsx
@@ -2,14 +2,14 @@ import { Headline } from "@telegram-apps/telegram-ui";
 import product1 from "@/assets/logo.png"
 
 const products = [
-  { img: product1, name: "Aveeno", description: "Baby Lotion", price: 100 },
-  { img: product1, name: "Shea Moisture", description: "Healing Lotion", price: 120 },
-  { img: product1, name: "Aveeno", description: "Baby Lotion", price: 100 },
-  { img: product1, name: "Shea Moisture", description: "Healing Lotion", price: 120 },
-  { img: product1, name: "Aveeno", description: "Baby Lotion", price: 100 },
-  { img: product1, name: "Shea Moisture", description: "Healing Lotion", price: 120 },
-  { img: product1, name: "Aveeno", description: "Baby Lotion", price: 100 },
-  { img: product1, name: "Shea Moisture", description: "Healing Lotion", price: 120 },
+  { img: product1, name: "Coming", description: "Soon..", price: 100 },
+  { img: product1, name: "Coming", description: "Soon..", price: 120 },
+  { img: product1, name: "Coming", description: "Soon..", price: 100 },
+  { img: product1, name: "Coming", description: "Soon..", price: 120 },
+  { img: product1, name: "Coming", description: "Soon..", price: 100 },
+  { img: product1, name: "Coming", description: "Soon..", price: 120 },
+  { img: product1, name: "Coming", description: "Soon..", price: 100 },
+  { img: product1, name: "Coming", description: "Soon..", price: 120 },
 ];
 
 
@@ -17,9 +17,9 @@ const ProductCard = ({ product }: {product: any}) => {
   return (
     <div className="p-2 rounded-lg shadow-md w-40">
       <img src={product.img} alt={product.name} className="w-full h-34 object-cover" />
-      <Headline className="text-sm font-semibold mt-2">{product.name}</Headline>
-      <p className="text-xs ">{product.description}</p>
-      <p className="text-blue-500 font-bold">${product.price}</p>
+      <Headline className="text-sm font-semibold mt-2">{product.name} <span className="text-xs ">{product.description}</span></Headline>
+      
+      {/* <p className="text-blue-500 font-bold">${product.price}</p> */}
       <button className="bg-blue-500 text-white text-xs py-1 px-2 rounded w-full mt-2">Add to Cart</button>
     </div>
   );
