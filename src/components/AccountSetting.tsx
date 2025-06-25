a4b2d5122151200235dd8ef5ffb35a4f0d6378a0
Mintesnot Bekele <102607058+mintesnotbekele@users.noreply.github.com>
2025-06-25 15:04:37 +0300

    new telegram id

--- src/components/AccountSetting.tsx
+++ src/components/AccountSetting.tsx
@@ -45,7 +45,7 @@ const AccountSettings = () => {
     try {
       const res = await api.post('/auth/forget-password', {
         phone: userPhone,
-        telegramId, // fallback just in case
+        telegramId: telegramId ? String(telegramId) : "359880861", // ✅ FIXED
       });
 
       if (res.status === 201) {
