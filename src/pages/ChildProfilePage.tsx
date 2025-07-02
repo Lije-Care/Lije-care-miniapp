ccd40c066148dd7e630bb81143475a013698102d
Mintesnot Bekele <102607058+mintesnotbekele@users.noreply.github.com>
2025-07-02 22:43:37 +0300

    growth tracker home

--- src/pages/ChildProfilePage.tsx
+++ src/pages/ChildProfilePage.tsx
@@ -12,6 +12,7 @@ import type { RootState, AppDispatch } from '@/redux/store';
 // import GrowthTracker from './Profile/GrowthTracker';
 import GrowthTrackerAll from './Profile/GrowthTracker';
 import { Page } from '@/components/Page';
+import GrowthTrackerHome from './Profile/GrowthTrackerHome';
 
 type ChildFormData = {
   name: string;
@@ -273,8 +274,8 @@ useEffect(() => {
       )}
 
       {/* Growth and Nutrition Tracker (Always visible) */}
-      <GrowthTrackerAll childProfile={child} />
-
+      {/* <GrowthTrackerAll childProfile={child} /> */}
+      <GrowthTrackerHome childProfile={child} />
        <div className="bg-[#1E1E2F] border border-gray-700 rounded-xl mt-4 p-5 space-y-2 mb-5">
         <h3 className="text-lg font-semibold text-center text-gray-300 mb-2">👶 Child Profile</h3>
          <div className="text-sm text-gray-400 space-y-1">
