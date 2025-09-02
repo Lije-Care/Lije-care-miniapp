6459e2c8774fbbb0c3d01e055219cf2253506f57
wallelignworkie <walleworkieas@gmail.com>
2025-09-02 15:58:04 +0300

    daily nutiration value addedd

--- src/pages/ecommerce/products/ProductList.tsx
+++ src/pages/ecommerce/products/ProductList.tsx
@@ -141,7 +141,7 @@ const ProductList = () => {
             Search
           </label>
           <div className="relative">
-            <div className="absolute inset-y-0 start-0 flex items-center ps-1 pointer-events-none">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-1 pointer-events-none ml-2 px-2">
               <svg
                 className="w-4 h-4 text-gray-500 dark:text-gray-400"
                 aria-hidden="true"
@@ -163,13 +163,13 @@ const ProductList = () => {
               id="default-search"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              className="block w-full p-2 ps-6 text-sm text-black border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500"
+              className="block w-[350px] p-2 ps-6 ml-2 text-sm text-white border border-gray-500 rounded-lg bg-gray-700 focus:ring-blue-500 focus:border-blue-500"
               placeholder="Search product..."
             />
 
             <button
               type="submit"
-              className=" mt-4 text-white absolute end-0.5 bottom-0.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className=" mt-4 text-white absolute end-0.5 bottom-0.5 bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-4   pt-1.5 pb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Search
             </button>
