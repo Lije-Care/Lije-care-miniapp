7873bab07ece40175c1df27d2ba49f6323eb742c
wallelignworkie <walleworkieas@gmail.com>
2025-10-08 12:35:45 +0300

    env chage

--- src/pages/Profile/AddChildForm.tsx
+++ src/pages/Profile/AddChildForm.tsx
@@ -163,7 +163,7 @@ const AddChildForm: React.FC<AddChildFormProps> = ({ onClose }) => {
                 <select
                   {...controllerField}
                   id={field.name}
-                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-teal-500 w-full bg-[#D9D9D94D] text-[#FFFFFF]"
+                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-teal-500 w-full bg-[#D9D9D94D] "
                 >
                   {field.options?.map((option) => (
                     <option key={option.value} value={option.value}>
