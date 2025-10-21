b2f7b9e333a32f7c62badec6b44da66851f281fd
wallelignworkie <walleworkieas@gmail.com>
2025-10-21 13:52:21 +0300

    chage bg color

--- src/pages/ChildrenListPage.tsx
+++ src/pages/ChildrenListPage.tsx
@@ -106,7 +106,7 @@ const ChildrenListPage: React.FC = () => {
   return (
     <Page back={true}>
       <div className="min-h-screen">
-        <div className="flex justify-between items-center mb-6 bg-[#0B364F] p-4">
+        <div className="flex justify-between items-center mb-6 bg-[#013222] p-4">
           <Headline className=" text-white">{t("My Children")}</Headline>
 
           <button
